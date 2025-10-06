13c31c06b604cdabe7111e3e250a125ff967d02d
wedfwef3 <eeeiqjjj239@gmail.com>
2025-10-06 19:07:22 -0400

    Update ere.lua

--- ere.lua
+++ ere.lua
@@ -1,19 +1,28 @@
-local p=game.Players.LocalPlayer
-local c=p.Character or p.CharacterAdded:Wait()
-local orig=c.PrimaryPart.Position
+local Players = game:GetService("Players")
+local p = Players.LocalPlayer
+local c = p.Character or p.CharacterAdded:Wait()
+local orig = c.PrimaryPart.Position
 
-for i=1,6 do
-    local m=workspace["FREEPEABERT"..i]
-    c:MoveTo(m.PrimaryPart.Position + Vector3.new(0,3,0))
-    task.wait(0.2)
-    for _,v in ipairs(m:GetDescendants()) do
-        if v:IsA("ProximityPrompt") then
-            v:InputHoldBegin()
-            task.wait(1.1)
-            v:InputHoldEnd()
+for i = 1, 6 do
+    local m = workspace:FindFirstChild("FREEPEABERT"..i)
+    if m then
+        local body = m:FindFirstChild("Body")
+        if body then
+            local part = body:FindFirstChild("Part")
+            if part and c.PrimaryPart then
+                c:MoveTo(part.Position + Vector3.new(0,3,0))
+                task.wait(0.2)
+                for _, v in ipairs(m:GetDescendants()) do
+                    if v:IsA("ProximityPrompt") then
+                        v:InputHoldBegin()
+                        task.wait(1.1)
+                        v:InputHoldEnd()
+                    end
+                end
+                task.wait(2)
+            end
         end
     end
-    task.wait(2)
 end
 
 c:MoveTo(orig)
